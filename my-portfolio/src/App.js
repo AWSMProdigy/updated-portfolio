c01eb886f976d19c36ed4d327efff955cae0bfd4
AWSMProdigy <StraightAKyle@gmail.com>
2022-04-29 23:50:50 -0400

    Navbar functionality present

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import React, { useState } from "react";
+
+
 
 function App() {
+  const [mobile, setMobile] = useState("closed")
+
+  const showMobile = () =>{
+    if(mobile === "open"){
+        setMobile("closed")
+    }else{
+      setMobile("open");
+    }
+  }
   return (
     <div className="App">
       <nav>
-        <div></div>
+        <div className="left"></div>
         <div className="name">
           <h1>Kyle Sousa</h1>
         </div>
-        <div>
+        <div className="right">
+          <i className="fa fa-solid fa-bars" onClick={showMobile} />
           <ul className="nav-links">
-            <li>About</li>
-            <li>About</li>
-            <li>About</li>
+            <a href="#About">About</a>
+            <a href="#Projects">Projects</a>
+            <a href="#Contact">Contact</a>
           </ul>
         </div>
-      </nav>
+        </nav>
+        <div className={"nav-links-mobile " + mobile} onClick={showMobile}>
+          <a href="#About"><h1>About</h1></a>
+          <a href="#Projects"><h1>Projects</h1></a>
+          <a href="#Contact"><h1>Contact</h1></a>
+        </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

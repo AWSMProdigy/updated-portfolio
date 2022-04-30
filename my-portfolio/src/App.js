5f5c8f3fae666f51277023bffc9606f5e56ac063
AWSMProdigy <StraightAKyle@gmail.com>
2022-04-30 06:38:20 -0400

    Reworking nav

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,29 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
-
-
 import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-
 
 function App() {
   const [mobile, setMobile] = useState("closed");
   
   const showMobile = () =>{
     const mobBtn = document.querySelector('.mob-btn');
+    const navLinks = document.querySelector('.nav-links')
     if(mobile === "open"){
         setMobile("closed")
+        navLinks.classList.remove("true")
         mobBtn.classList.remove('open');
 
     }else{
         setMobile("open");
+        navLinks.classList.add("true")
         mobBtn.classList.add('open');
     }
   }
@@ -35,14 +28,14 @@ function App() {
           <h1>Kyle Sousa</h1>
         </div>
         <div className="right">
-          <div className='mob-btn' onClick={showMobile}>
-            <div className="mob-btn__burger"></div>
-          </div>
           <ul className="nav-links">
             <a href="#About">About</a>
             <a href="#Projects">Projects</a>
             <a href="#Contact">Contact</a>
           </ul>
+          <div className='mob-btn' onClick={showMobile}>
+            <div className="mob-btn__burger"></div>
+          </div>
         </div>
         </nav>
         <div className={"nav-links-mobile " + mobile} onClick={showMobile}>

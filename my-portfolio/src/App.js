c0a86487164009c2493a277df495add4301bd786
AWSMProdigy <StraightAKyle@gmail.com>
2022-05-15 23:22:08 -0400

    Added space for 3 projects

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -322,6 +322,9 @@ function App() {
               <p>Uses ReactJS, GraphQL, and MongoDB</p>
             </div>
           </div>
+          <div className='project'>Reel Cinema</div>
+          <div className='project'>Daily Felon</div>
+          <div className='project'>Twitter Bot</div>
         </div>
       </div>
       <div className="Page Contact" id="Contact">

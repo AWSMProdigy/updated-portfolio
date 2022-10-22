9787cade0a09a9c76a328d6191fe3bec1a4c520c
AWSMProdigy <StraightAKyle@gmail.com>
2022-10-22 15:39:53 -0400

    Added github and website links to project page

--- src/components/Project.js
+++ src/components/Project.js
@@ -22,11 +22,16 @@ function Project(){
                     </p>
                 </div>
             </div>
-                <div className="techStack">
-                    <h1>Tech Stack:</h1>
-                    <div className="iconList" dangerouslySetInnerHTML={{__html: stack}}>
-                    </div>
+            <div className="iconList">
+                <li><a href={data.state.github}><i className='fa fa-github fa-4x'></i></a></li>
+                <li><a href={data.state.github}><i className='fa fa-globe fa-4x'></i></a></li>
+
+            </div>
+            <div className="techStack">
+                <h1>Tech Stack:</h1>
+                <div className="iconList" dangerouslySetInnerHTML={{__html: stack}}>
                 </div>
+            </div>
         </div>
     )
 }
